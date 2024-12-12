10fcb8ea6c3dff3f927cf4937a80e57f56cbe7ea
alancalderondev <alancalderondev@gmail.com>
2024-12-11 20:19:09 -0600

    Inicializacion del Section con Lit

---
+++ components/JS/section.js
@@ -0,0 +1,64 @@
+import {html, css, LitElement} from 'lit';
+
+class Section extends LitElement{
+
+    static styles = css`
+        a{
+            text-decoration: none;
+            display:flex;
+        }
+        ul{
+            list-style: none;
+            padding: 0;
+        }
+        img{
+            max-width: 50px;
+            margin-right: 10px;
+            display: inline-block;
+        }
+        .elementos{
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+        }
+    `;
+
+    static properties = {
+        data: {type: Array}
+    }
+    
+    constructor(){
+        super();
+        this.data = [];//Aseguramos que data esta inicializado
+    }
+    //RENDERIZAR
+    render(){
+        //Verifico que mi arreglo no este vacio
+        if(this.data.length >0){
+            //Aqui proceso lo del DATA recorriendolo con el MAP
+            return html`
+                <ul>
+                    <!--Recorremos nuestro Arreglo con .map -->
+                    ${this.data.map(datos =>
+                        html`
+                        <li>
+                            <div class="elementos">
+                                <a href="#">
+                                    <img src="${datos.src}"/>
+                                    <p>${datos.name}</p>
+                                </a>
+                            </div>
+                        </li>
+                        `
+                    )}
+                </ul>
+            `;
+        }else{
+            return html`
+                <p>No hay elementos en el arreglo</p>
+            `;
+        }
+    }
+}
+
+customElements.define('seccion-principal',Section);
